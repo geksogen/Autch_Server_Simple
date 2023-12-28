b69bfff3b7849aedb1efb9136129d061349f32d9
Andrei <test@test.mail>
2023-12-28 11:11:36 +0300

    fix client.py

--- Client/client.py
+++ Client/client.py
@@ -87,4 +87,4 @@ if __name__ == '__main__':
   #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
   #context.load_cert_chain('domain.crt', 'domain.key')
   #app.run(port = 5000, debug = True, ssl_context = context)
-  app.run(port = 5000, debug = True)
+  app.run(port = 5000, debug = True, host = '62.84.115.92')
