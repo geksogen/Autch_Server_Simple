1f48387124ed8ac28cea38961bd4317c9306ad98
Andrei <test@test.mail>
2023-12-28 14:04:41 +0300

    add API server

--- Client/client.py
+++ Client/client.py
@@ -6,10 +6,10 @@ import os
 from flask import (Flask, make_response, render_template, redirect, request,
                    url_for)
 
-AUTH_PATH = 'http://autch_server_my:5001/auth'
-TOKEN_PATH = 'http://autch_server_my:5001/token'
+AUTH_PATH = 'http://158.160.116.18:5001/auth'
+TOKEN_PATH = 'http://158.160.116.18:5001/token'
 RES_PATH = 'http://158.160.116.18:5002/users'
-REDIRECT_URL = 'http://client_my:5000/callback'
+REDIRECT_URL = 'http://158.160.116.18:5000/callback'
 
 CLIENT_ID = 'sample-client-id'
 CLIENT_SECRET = 'sample-client-secret'
